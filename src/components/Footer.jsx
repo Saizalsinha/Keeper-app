81329fbdc208588ace9eb879a00d8a26e405e58e
Saizalsinha <saizalsinha2003@gmail.com>
2023-02-09 00:40:14 +0530

    Make Basic Structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+
+    return (<footer>
+        <p>Copyright ⓒ {currentYear}</p>
+    </footer>);
+}
+
+export default Footer;
